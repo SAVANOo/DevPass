ef051ece6660c5caf9cfad13d391a7768a983e5b
Araujomann <lucastheeyepatch@gmail.com>
2024-06-12 14:34:45 -0300

    fix: little adjustments

--- Pages/CourseList/script.js
+++ Pages/CourseList/script.js
@@ -1,54 +1,66 @@
-document.addEventListener('DOMContentLoaded', () => {
+import { baseURL } from "../../config.js";
+const accessToken = localStorage.getItem("token");
+if (!accessToken) window.location.href = "../LoginPage/Index.html";
+
+document.addEventListener("DOMContentLoaded", () => {
     // Define the API endpoint
-    const apiEndpoint = 'http://devpass-backend-production.up.railway.app/api/v1/cursos/cursos';
 
     // Func. de dar fetch na API
     function fetchCourses() {
-        fetch(apiEndpoint)
-            .then(response => {
+        fetch(`${baseURL}/cursos/cursos`, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                Autorization: `Bearer ${accessToken}`,
+            },
+        })
+            .then((response) => {
                 if (response.ok) {
                     return response.json();
                 } else {
-                    throw new Error('Network response was not ok');
+                    throw new Error("Network response was not ok");
                 }
             })
-            .then(data => {
+            .then((data) => {
                 displayCourses(data);
             })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
+            .catch((error) => {
+                console.error(
+                    "There was a problem with the fetch operation:",
+                    error
+                );
             });
     }
 
     // Manipulação DOM
     function displayCourses(courses) {
-        const videosContainer = document.querySelector('.videos-container');
+        const videosContainer = document.querySelector(".videos-container");
 
         // Tirar o que tem no HTML
-        videosContainer.innerHTML = '';
+        videosContainer.innerHTML = "";
 
-        courses.forEach(course => {
+        courses.forEach((course) => {
             // Criar 1 por 1
-            const courseItem = document.createElement('div');
-            courseItem.classList.add('course-item');
+            const courseItem = document.createElement("div");
+            courseItem.classList.add("course-item");
 
             /* const courseImg = document.createElement('img');
             courseImg.src = course.thumbnail || 'Src/thumbnail.jpg';
             courseImg.alt = course.title; // Ainda não foi implementado as thumbs na API */
 
-            const courseInfo = document.createElement('div');
-            courseInfo.classList.add('course-info');
+            const courseInfo = document.createElement("div");
+            courseInfo.classList.add("course-info");
 
-            const courseTitle = document.createElement('h3');
+            const courseTitle = document.createElement("h3");
             courseTitle.textContent = course.titulo;
 
-            const courseInstructor = document.createElement('p');
+            const courseInstructor = document.createElement("p");
             courseInstructor.textContent = course.professor;
 
-            const courseRating = document.createElement('p');
+            const courseRating = document.createElement("p");
             courseRating.textContent = `${course.rating} ★★★★☆`; // Rating fixa; ainda não tem implementação de rating dinâmica na API
 
-            const coursePrice = document.createElement('p');
+            const coursePrice = document.createElement("p");
             coursePrice.textContent = `R$ ${course.preco.toFixed(2)}`;
 
             // Append elements to course info
@@ -67,7 +79,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // Pegar todos os cursos com o botão de verTudo
-    document.querySelector('.verTudo').addEventListener('click', () => {
+    document.querySelector(".verTudo").addEventListener("click", () => {
         fetchCourses();
     });
 
