56176cd5feef61e6183cbc86acea83c89a421482
SAVANOo <savio_gato1000@hotmail.com>
2024-06-13 21:41:58 -0300

    consumindo api corretamente (vsf matheuzim)

--- Pages/ComunityPage/script.js
+++ Pages/ComunityPage/script.js
@@ -2,28 +2,24 @@ const accessToken = localStorage.getItem('token');
 
 if (!accessToken) window.location.href = "../LoginPage/Index.html";
 
-function buildComunity(titulo, descricao){
-  const comunidadeItem = document.createElement('div'); 
-  const comunidadeItens = document.querySelector(".comunity-cards");
-  
-      comunidadeItem.classList.add("card");
-          comunidadeItem.innerHTML=`
-                            <div class="card-content">
-                                <img class="icon-card" src="/Pages\ComunityPage\Src\Comunidades\Imagens\iconcomunidade.png">
-                                <h1 class="card-title">Xeloguers</h1>
-                                <p class="card-text">Comunidade dos Xeloguers. O Ruan sempre tá errado.</p>
-                          
+function buildComunityItem(mapData, bgColor) {
+    const comunidadeCardsContainer = document.querySelector(".comunity-cards");
+    const comunidadeCardItem = document.createElement('div');
+    comunidadeCardItem.classList.add("card");
+    comunidadeCardItem.classList.add(`${bgColor}-background`);
+    comunidadeCardItem.innerHTML = `
+                        <div class="card-content">
+                            <img class="icon-card" src="/Pages/ComunityPage/Src/Comunidades/Imagens/iconcomunidade.png">
+                            <h1 class="card-title">${mapData.nome}</h1>
+                            <p class="card-text">${mapData.tema}</p>
                             <button class="saiba-mais">Saiba Mais</button>
                         </div>
                     `
-
-            comunidadeItens.appendChild(comunidadeItem);
-
-            console.log(comunidadeItem);
+    comunidadeCardsContainer.appendChild(comunidadeCardItem);
 }
 
 
-fetch(`${baseUrl}/comunidades`,{
+fetch(`${baseUrl}/comunidades`, {
     method: 'GET',
     headers: {
         'Accept': 'application/json',
@@ -38,26 +34,13 @@ fetch(`${baseUrl}/comunidades`,{
         }
     })
     .then(data => {
-        console.log('Comunidades', data);
-        let qntCards = 4;  // Number of cards to display
-        let colors = ["blue", "orange", "green", "grey"];
-        let bgColor = {
-            blue: "#0500ff",
-            orange: "#ff8a00",
-            green: "#05ff00",
-            grey: "#8a8a8a"
-        };
+        let colors = ["blue", "orange", "green", "grey"]
+        for (i = 0; i < 4; i++) {
+            buildComunityItem(data[i], colors[i]);
+        }
 
-        data.forEach((element, index) => {
-            if (qntCards > 0) {
-                let color = colors[index % 4];  // Cycle through the colors
-                buildComunity(element.nome, element.descricao);
-                qntCards--;
-            }
-            console.log(element);
-        });
     })
     .catch(error => {
         console.error('Erro na requisição:', error);
-        // window.location.href = "../LoginPage/Index.html";
+        window.location.href = "../LoginPage/Index.html";
     });
