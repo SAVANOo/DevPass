5494465cb0239f8f6fc93741750c28291607f0ae
Matheus Lima <limarodrigues660@gmail.com>
2024-06-17 23:29:38 -0300

    feat: Modularization and API integration on coursepage.

--- Pages/CoursePage/script.js
+++ Pages/CoursePage/script.js
@@ -1,102 +1,204 @@
-const containers = document.querySelectorAll('.ContainerItems');
+import * as courseCarousel from  "../Components/courseCarousel.js";
 
-// Função para adicionar funcionalidade de arrastar e rolagem a um único elemento
-function addDragAndScrollFunctionality(container) {
-    let isDragging = false;
-    let startPositionX = 0;
+const accessToken = localStorage.getItem('token');
 
-    container.addEventListener('mousedown', (e) => {
-        isDragging = true;
-        startPositionX = e.clientX;
-    });
+if (accessToken) {
 
-    container.addEventListener('mousemove', (e) => {
-        if (!isDragging) return;
-        const offsetX = e.clientX - startPositionX;
-        container.scrollLeft -= offsetX;
-        startPositionX = e.clientX;
-    });
+    const urlParams = new URLSearchParams(window.location.search);
+    const idParameter = urlParams.get('id');
+    console.log(idParameter); // 42
 
-    container.addEventListener('mouseup', () => {
-        isDragging = false;
-    });
-
-    container.addEventListener('mouseleave', () => {
-        isDragging = false;
-    });
-}
-
-// Itere sobre os elementos e adicione a funcionalidade a cada um
-containers.forEach(container => {
-    addDragAndScrollFunctionality(container);
-});
-
-
-const conteudos = {
-    Vagas: document.querySelector("#ConteudoVagas"),
-    jsonUrlVagas: "Src/Vagas/data.json",
-
-    Comunidades: document.querySelector("#ConteudoComunidades"),
-    jsonUrlComunidades: "Src/Comunidades/data.json",
-
-    Noticias: document.querySelector("#ConteudoNoticias"),
-    jsonUrlNoticias: "Src/NoticiasDev/data.json"
-};
-
-// Função para buscar e atualizar os dados
-function fetchData(container, jsonUrl) {
-    fetch(jsonUrl)
-        .then((response) => response.json())
-        .then((data) => {
-            // Limpar o conteúdo atual da div
-            container.innerHTML = "";
-
-            // Iterar sobre os dados e criar elementos HTML
-            data.forEach((item) => {
-                const itemDiv = document.createElement("div");
-                itemDiv.classList.add("Items");
-
-                const img = document.createElement("img");
-                img.src = item.imagem;
-                img.alt = "ImagemItem";
-
-                const link = document.createElement("a");
-                link.href = item.link;
-                link.classList.add("VagaTitle");
-                link.textContent = item.titulo;
-                link.setAttribute("target", "_blank");
-
-
-
-                itemDiv.appendChild(img);
-                itemDiv.appendChild(link);
-
-                container.appendChild(itemDiv);
-            });
+    /* Requisição para receber coleção de cursos*/
+    fetch(`${baseUrl}/cursos/cursos`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${accessToken}`
+        }
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Falha ao acessar o recurso protegido. Por favor, faça login novamente.');
+            }
         })
-        .catch((error) => {
-            console.error("Ocorreu um erro ao buscar os dados: ", error);
+        .then(data => {
+            console.log(data);
+            data.cursos.forEach(course => {
+                if (course.id == idParameter)
+                    buildCoursePage(course);
+            })
+
+            let qntItems = 10;
+            courseCarousel.buildCardCourse(data.cursos, qntItems);
+            courseCarousel.enableCourseCarousel();
+        })
+        .catch(error => {
+            console.error('Erro na requisição:', error);
+            /* window.location.href = "../LoginPage/Index.html"; */
         });
+
+
+} else {
+    window.location.href = "../LoginPage/Index.html";
 }
 
-// Chame a função para buscar e atualizar os dados
-fetchData(conteudos.Vagas, conteudos.jsonUrlVagas);
-fetchData(conteudos.Comunidades, conteudos.jsonUrlComunidades);
-fetchData(conteudos.Noticias, conteudos.jsonUrlNoticias);
+function buildCoursePage(course) {
+    console.log(course)
+    const headerCourse = document.querySelector(".Header-course");
+    headerCourse.innerHTML = '';
+
+    const headerContent = document.createElement("div");
+    headerContent.classList.add("Content");
+
+    const informationKeyContent = document.createElement("div");
+    informationKeyContent.classList.add("Information-key");
+
+    /* Definição de título, sub-título, autor, avaliação e idioma */
+    const courseImage = document.createElement("img");
+    courseImage.classList.add("BackgroundImage1");
+    courseImage.src = course.thumb; //fixo no momento
+
+    const courseTitle = document.createElement('h1');
+    courseTitle.classList.add("Title");
+    courseTitle.textContent = course.titulo;
+
+    const courseSubTitle = document.createElement("p")
+    courseSubTitle.classList.add("Sub-title");
+    courseSubTitle.textContent = course.descricao.split(".")[0] + ".";
+
+    const courseAuthor = document.createElement("p");
+    courseAuthor.classList.add("Autor");
+    courseAuthor.textContent = `Criado por ${course.professor}`;
+
+    const courseRating = document.createElement("div");
+    courseRating.classList.add("Rating");
+
+    const valueRating = document.createElement("span");
+    valueRating.classList.add("yellow");
+    valueRating.textContent = `${course.rating},0 `; //fixo no momento
+    courseRating.appendChild(valueRating);
+
+    let arrayStar = ['', '', '', '', '']
+    arrayStar.forEach((element, index) => {
+        if (index < course.rating) {
+            element = '<i class="fa-solid fa-star yellow"></i>';
+        } else {
+            element = '<i class="fa-solid fa-star gray"></i>';
+        }
+        courseRating.innerHTML += element;
+    });
+
+    const courseLanguage = document.createElement("div");
+    courseLanguage.classList.add("Language");
+
+    const iconLanguage = document.createElement("i");
+    iconLanguage.setAttribute("class", "fa-solid fa-globe");
+
+    const nameLanguage = document.createElement("span");
+    nameLanguage.textContent = "Português";
 
 
-function performSearch() {
-    const searchTerm = searchInput.value;
-    const redirectURL = '../SearchPage/Index.html' + `?search=${encodeURIComponent(searchTerm)}`;
-    window.location.href = redirectURL;
+    /* Criação do card do curso */
+    const courseCard = document.createElement("div");
+    courseCard.classList.add("Course-card");
+
+    const cardImage = document.createElement("img");
+    cardImage.src = course.thumb;
+
+    const iconPlay = document.createElement("i");
+    iconPlay.setAttribute("class", "fa-regular fa-circle-play fa-2xl");
+
+    const coursePriceContent = document.createElement("div");
+    coursePriceContent.classList.add("Value");
+    coursePriceContent.innerHTML = `<div id="value">R$ ${course.preco}</div>`
+
+    const cardButtonContent = document.createElement("div");
+    cardButtonContent.classList.add("Button-content");
+
+    const cardButton = document.createElement("button");
+    cardButton.id = "buy-button";
+    cardButton.textContent = "Comprar Curso"
+    cardButton.addEventListener("click", () => { window.location.href = `../CourseVideoPage/index.html?id=${course.id}` })
+
+    headerCourse.appendChild(courseImage);
+    informationKeyContent.appendChild(courseTitle);
+    informationKeyContent.appendChild(courseSubTitle);
+    informationKeyContent.appendChild(courseAuthor);
+    informationKeyContent.appendChild(courseRating);
+    courseLanguage.appendChild(iconLanguage);
+    courseLanguage.appendChild(nameLanguage);
+    informationKeyContent.appendChild(courseLanguage);
+    courseCard.appendChild(cardImage);
+    courseCard.appendChild(iconPlay);
+    courseCard.appendChild(coursePriceContent);
+    cardButtonContent.appendChild(cardButton);
+    courseCard.appendChild(cardButtonContent)
+    headerContent.appendChild(informationKeyContent);
+    headerContent.appendChild(courseCard);
+    headerCourse.appendChild(headerContent);
+
+    /* Construção da descrição */
+
+    const descriptionContent = document.querySelector(".Description");
+
+    const descriptionTitle = document.createElement("h2");
+    descriptionTitle.textContent = "Descrição";
+
+    const descriptionText = document.createElement("p");
+    descriptionText.textContent = course.descricao;
+
+    /* Construção do conteúdo */
+
+    const courseContent = document.querySelector(".Course-content");
+
+    const contentTitle = document.createElement("h2");
+    contentTitle.textContent = "Conteudo";
+
+    const contentFrame = document.createElement("div");
+    contentFrame.classList.add("Frame");
+
+    const frameHeader = document.createElement("div");
+    frameHeader.classList.add("Frame-header");
+
+    const frameChapter = document.createElement("div");
+    frameChapter.classList.add("Chapter");
+    frameChapter.innerHTML = `<i class="fa-solid fa-circle-arrow-down"></i><span>INTRODUÇÃO</span>`;
+
+    const contentInfo = document.createElement("div");
+    contentInfo.classList.add("Content-info");
+
+    const listContent = document.createElement("ul");
+
+    //forma dinâmica, esperando o back ser implementado
+    /* course.conteudo.split(".").forEach((element) => {
+        listContent.innerHTML += `<li>${element}</li>`;
+        }) */
+
+    listContent.innerHTML += `<li>Aula 1: Introdução ao curso</li>
+       <li>Aula 2: Objetivos do curso</li>
+       <li>Aula 3: Estrutura do curso</li>`
+
+    descriptionContent.appendChild(descriptionTitle);
+    descriptionContent.appendChild(descriptionText);
+    courseContent.appendChild(contentTitle);
+    frameHeader.appendChild(frameChapter);
+    frameHeader.innerHTML += `<p>${course.duracao}</p>`;
+    contentFrame.appendChild(frameHeader);
+    contentInfo.appendChild(listContent);
+    contentFrame.appendChild(contentInfo);
+    courseContent.appendChild(contentFrame);
+
+    /* Abrir e recolher a barra de conteúdo */
+    contentFrame.addEventListener("click", () => { toggleContent(contentInfo) });
 }
 
-const searchButton = document.getElementById("searchButton");
-searchButton.addEventListener("click", performSearch);
-
-const searchInput = document.getElementById("desktopSearchInput");
-searchInput.addEventListener("keyup", function (event) {
-    if (event.key === "Enter") {
-        performSearch();
+function toggleContent(element) {
+    console.log(element)
+    if (element.style.display === "flex") {
+        element.style.display = "none";
+    } else {
+        element.style.display = "flex";
     }
-});
+}
